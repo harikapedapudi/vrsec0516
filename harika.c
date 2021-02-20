73b8382e733c26733ab8d008994863681ac58918
harikapedapudi <harikapedapudi279@gmail.com>
2021-02-20 10:28:38 +0530

    local modifications

--- harika.c
+++ harika.c
@@ -2,8 +2,7 @@
 void main()
 { 
   printf("welcome to workshop");
-  printf("add=%d",10+20);
-  printf("i have been successfully added two numbers");
   printf("APSSDC-VRSEC");
   printf("-----------day2-------");
+  printf("modification in local system")
 }
